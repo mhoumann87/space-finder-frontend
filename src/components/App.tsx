9a67ac9a6f29541701a11ffc56c5c56a5079ea57
Michael Houmann <mhoumann87@gmail.com>
2021-09-21 08:32:30 +0200

    Setup router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+
+import history from '../utils/history';
 import { User } from '../model/User';
 import { AuthService } from '../services/AuthService';
 import { Login } from './Login';
+import { NavBar } from './NavBar';
+import { Home } from './Home';
+import { Profile } from './Profile';
+
 interface AppState {
   user: User | undefined;
 }
@@ -11,6 +18,10 @@ export class App extends React.Component<{}, AppState> {
 
   constructor(props: any) {
     super(props);
+
+    this.state = {
+      user: undefined,
+    };
 
     this.setUser = this.setUser.bind(this);
   }
@@ -24,9 +35,21 @@ export class App extends React.Component<{}, AppState> {
 
   render() {
     return (
-      <div>
-        <h1>Class Component</h1>
-        <Login authService={this.authService} setUser={this.setUser} />
+      <div className='wrapper'>
+        <Router history={history}>
+          <div>
+            <NavBar user={this.state.user} />
+            <Switch>
+              <Route exact path='/' component={Home} />
+
+              <Route exact path='/login'>
+                <Login authService={this.authService} setUser={this.setUser} />
+              </Route>
+
+              <Route exact path='/profile' component={Profile} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
